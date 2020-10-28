8ad10b70cf117c84052ecf02cf5a650d53f5ee45
Huy Vuong <hnvuong@speedwaymotors.com>
2020-10-28 14:42:52 -0500

    update project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
-def main():
-    pass
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import os
+import sys
+from numerical_integration import left_right_sum, trapezoid_sum, trapezoid_sum_error, simpson_18rule
 
-if __name__ == "__main__":
-    main()
+# %%
+f = lambda x: x ** 2
+xs = [1,2,3,4,5]
+left_sum, right_sum = left_right_sum(f, (2, 3), 5)
+print(left_sum, right_sum)
+
+# %%
+trapezoid = trapezoid_sum(f, (2, 3), 5)
+print(trapezoid)
+f_derived = lambda x : 2
+epsilon = trapezoid_sum_error(f, f_derived, (2, 3), 5)
+print(epsilon)
+
+#%%
+simpsons = simpson_18rule(f, (2, 3), 6)
+print(simpsons)
+
+
+
+# %%
---
+++ src/numerical_integration.py
@@ -0,0 +1,75 @@
+from typing import Callable, List, Tuple
+import random
+
+def left_right_sum(f: Callable[[float], float],
+                   interval: Tuple[float, float],
+                   n_subintervals: int) -> (float, float):
+    a, b = interval
+    x_delta = (b - a) / n_subintervals
+
+    ys = [f(a + x_delta * i) for i in range(n_subintervals + 1)]
+    left_sum = x_delta * sum(ys[:len(ys) - 1])
+    right_sum = x_delta * sum(ys[1:])
+
+    return (left_sum, right_sum)
+
+def trapezoid_sum(f: Callable[[float], float],
+                  interval: Tuple[int, int],
+                  n_subintervals: int) -> float:
+    a, b = interval
+    h = (b - a) / n_subintervals
+
+    ys = [f(a + h * i) for i in range(n_subintervals + 1)]
+    trapezoids = 1 / 2 * h * (f(a) + f(b) + 2 * sum([f(a + h * i) for i in range(1, n_subintervals)]))
+
+    return trapezoids
+
+def simpson_18rule(f: Callable[[float], float],
+                   interval: Tuple[int, int],
+                   n_subintervals: int) -> float:
+    a, b = interval
+    h = (b - a) / n_subintervals
+    print(h)
+
+    spline = 2 * sum(f(a + 2 * k * h) for k in range(1, int((n_subintervals - 2) / 2) + 1)) \
+           + 4 * sum(f(a + (2 * k - 1) * h) for k in range(1, int(n_subintervals / 2) + 1))
+    simpsons = h / 3 * (f(a) + f(b) + spline)
+    
+    return simpsons
+
+def simpson_38rule(f: Callable[[float], float],
+                   interval: Tuple[int, int],
+                   n_subintervals: int) -> float:
+
+    pass
+
+def trapezoid_sum_error(f: Callable[[float], float],
+                        f_derived: Callable[[float], float],
+                        interval: Tuple[int, int],
+                        n_subintervals: int) -> float:
+    a, b = interval
+    xi, _ = find_max(f, interval)
+    error =  -1 * (b - a) ** 3 / (12 * n_subintervals ** 2) * f_derived(xi)
+
+    return error
+
+def simpson18_error(f: Callable[[float], float],
+                    f_derived: Callable[[float], float],
+                    interval: Tuple[int, int],
+                    n_subintervals: int) -> float:
+    a, b = interval
+    xi, _ = find_max(f, interval)
+    h = (b - a) / n_subintervals
+    error =  1 / 180 * (b - a) * h ** 4 * f_derived(xi)
+
+    return error
+
+
+def find_max(f: Callable[[float], float],
+             interval: Tuple[int, int],
+             steps: float = 0.0001) -> float:
+             
+    a, b = interval
+    fs = [(a + i * steps, f(a + i * steps)) for i in range(int((b - a) / steps) + 1)]
+
+    return max(fs, key=lambda t: t[1])
